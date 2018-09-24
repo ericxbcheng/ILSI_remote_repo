c1f9ab9369d85ac6cf85e2c784b0eac68d30c647
ericxbcheng <xianbin2@illinois.edu>
2018-09-24 10:55:29 -0500

    Update functions to include sampling plans "strs" and "ss"

--- Sampling_outcome.R
+++ Sampling_outcome.R
@@ -51,13 +51,13 @@ calc_ROD = function(df_cover, df_contam, n_sp, spread){
 }
 
 # Create a function that runs the simulation once and gives an ROD
-sim_outcome = function(n_contam, xlim, ylim, n_affected, covar_mat, spread_radius, n_sp, sp_radius, spread){
+sim_outcome = function(n_contam, xlim, ylim, n_affected, covar_mat, spread_radius, method, n_sp, sp_radius, spread, n_strata, by){
   
   # Generate the coordinates of contamination points
   contam_xy = sim_contam(n_contam = n_contam, xlim = xlim, ylim = ylim, covariance = covar_mat, n_affected = n_affected, radius = spread_radius) 
   
   # Generate the coordinates of sample points
-  sp_xy = sim_plan(n_sp = n_sp, xlim = xlim, ylim = ylim, r = sp_radius)
+  sp_xy = sim_plan(method = method, n_sp = n_sp, xlim = xlim, ylim = ylim, r = sp_radius, n_strata = n_strata, by = by)
   
   # Generate the combined coordinates of contamination and sample points
   contam_sp_xy = rbind(contam_xy, sp_xy)
