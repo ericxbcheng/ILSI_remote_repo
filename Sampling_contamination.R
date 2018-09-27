ecae52d230c825bed64fecfb21fb0241306a846b
ericxbcheng <xianbin2@illinois.edu>
2018-09-27 15:44:26 -0500

    Fix a bug that occurs when n_affected = 0 in sim_contam()

--- Sampling_contamination.R
+++ Sampling_contamination.R
@@ -21,19 +21,8 @@ sim_contam = function(n_contam, xlim, ylim, covariance, n_affected, radius){
   x = runif(n = n_contam, min = xlim[1], max = xlim[2])
   y = runif(n = n_contam, min = ylim[1], max = ylim[2])
   
-  spot_coord = matrix(data = c(x, y), ncol = 2) %>% as.data.frame()
-  
-  # Each column of spread_coord contains 2*n_affected elements, the first n_affected elements are random numbers generated from mat_1[1,1] and the second n_affected elements are random numbers generated from mat_1[1,2]
-  spread_coord = apply(X = spot_coord, MARGIN = 1, FUN = mvrnorm, n = n_affected, Sigma = covar_mat)
-  
-  # Split the spread_coord by columns, rearrange the vector of each list into an nx2 matrix, then combine these matrices by row.
-  spread_coord_2 = split(x = spread_coord, f = col(spread_coord))
-  spread_coord_3 = lapply(X = spread_coord_2, FUN = function(x){matrix(x, ncol = 2)})
-  spread_coord_4 = do.call(what = rbind, args = spread_coord_3) %>% as.data.frame()
-  
-  # Give a unique identifier to each observation.
-  spread_coord_5 = naming_spread(spread_coord_4, n_contam, n_affected)
-  spot_coord_2 = naming_spot(spot_coord, n_contam)
+  spot_coord = matrix(data = c(x, y), ncol = 2) %>% 
+    as.data.frame()
   
   # Create a factor column.
   label = c(rep("spot", times = n_contam), rep("spread", times = n_contam * n_affected))
@@ -41,13 +30,43 @@ sim_contam = function(n_contam, xlim, ylim, covariance, n_affected, radius){
   # Create a spread radius column
   r = spread_radius
   
-  # Combine the contamination spot coordinates and the spread coordinates, and then combine the factor column and the spread radius column.
-  df_1 = rbind(spot_coord_2, spread_coord_5)
-  colnames(df_1) = c("X", "Y", "ID")
-  df_2 = cbind(df_1, label, r)
+  if(n_affected != 0){
+    # Each column of spread_coord contains 2*n_affected elements, the first n_affected elements are random numbers generated from mat_1[1,1] and the second n_affected elements are random numbers generated from mat_1[1,2]
+    spread_coord = apply(X = spot_coord, MARGIN = 1, FUN = mvrnorm, n = n_affected, Sigma = covar_mat)
+    
+    # Split the spread_coord by columns, rearrange the vector of each list into an nx2 matrix, then combine these matrices by row.
+    spread_coord_2 = spread_coord %>%
+      split(x = ., f = col(.)) %>%
+      map(.x = ., .f = function(x) {matrix(x, ncol = 2)}) %>%
+      do.call(what = rbind, args = .) %>%
+      as.data.frame()
+    
+    # Give a unique identifier to each observation.
+    spread_coord_3 = naming_spread(spread_coord_2, n_contam, n_affected)
+    spot_coord_2 = naming_spot(spot_coord, n_contam)
+    
+    # Combine the contamination spot coordinates and the spread coordinates, and then combine the factor column and the spread radius column.
+    df_1 = rbind(spot_coord_2, spread_coord_3)
+    colnames(df_1) = c("X", "Y", "ID")
+    df_2 = cbind(df_1, label, r)
+    
+  } else {
+    
+    # Give a unique identifier to each observation.  
+    spot_coord_2 = naming_spot(spot_coord, n_contam)
+    
+    # Give the spot_coord_2 labels and radius column
+    df_1 = spot_coord_2
+    colnames(df_1) = c("X", "Y", "ID")
+    df_2 = cbind(df_1, label, r)
+    
+  }
   
   # Remove points that are outside the perimeter.
-  out = which(df_2[ ,1] < xlim[1] | df_2[ , 1] > xlim[2] | df_2[,2] < ylim[1] | df_2[,2] > ylim[2], arr.ind = TRUE)[1]
+  out = which(df_2[ ,1] < xlim[1] | 
+                df_2[ , 1] > xlim[2] | 
+                df_2[,2] < ylim[1] | 
+                df_2[,2] > ylim[2], arr.ind = TRUE)[1]
   if(is.na(out) == FALSE){
     df_3 = df_2[-out, ]
   } else {
