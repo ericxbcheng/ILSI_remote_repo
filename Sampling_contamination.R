2d09a004777499af81bacdccf051abfa870555a3
ericxbcheng <xianbin2@illinois.edu>
2018-10-09 11:43:22 -0500

    Use logical subsetting instead of which() to remove outliers in sim_contam()

--- Sampling_contamination.R
+++ Sampling_contamination.R
@@ -106,15 +106,12 @@ sim_contam = function(n_contam, xlim, ylim, covariance, n_affected, radius, cont
   }
   
   # Remove points that are outside the perimeter.
-  out = which(df_2$X < xlim[1] | 
-                df_2$X > xlim[2] | 
-                df_2$Y < ylim[1] | 
-                df_2$Y > ylim[2])
-  if(is.na(out) == FALSE){
-    df_3 = df_2[-out, ]
-  } else {
-    df_3 = df_2
-  }
+  out = df_2$X < xlim[1] |
+    df_2$X > xlim[2] |
+    df_2$Y < ylim[1] |
+    df_2$Y > ylim[2]
+  
+  df_3 = df_2[!out, ]
   
   # Reset the row names
   rownames(df_3) = NULL
