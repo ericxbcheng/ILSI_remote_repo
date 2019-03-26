ebc776643deedeea90cbbdab74413d5efa6cb73d
ericxbcheng <xianbin2@illinois.edu>
2019-03-26 17:26:18 -0500

    Create sim_outcome_cont(); Replace argument "d" with "sp_radius" in sim_intmed_dis() and sim_intmed()

--- Sampling_outcome_3d.R
+++ Sampling_outcome_3d.R
@@ -1,13 +1,13 @@
 # Create intermediate datasets for discrete case
-sim_intmed_dis = function(n_contam, lims, spread, covar_mat, n_affected, spread_radius, dis_level, 
-                          method_sp, d, container, compartment, type, L, rho, m_kbar, conc_neg, tox){
+sim_intmed_dis = function(n_contam, lims, spread, covar_mat, n_affected, dis_level, 
+                          method_sp, sp_radius, container, compartment, type, L, rho, m_kbar, conc_neg, tox){
   
   # Generate the coordinates of contamination points
   contam_xy = sim_contam_new(n_contam = n_contam, lims = lims, spread = spread, covar = covar_mat, 
                              n_affected = n_affected, spread_radius = NA, dis_level = dis_level) 
   
   # Generate the coordinates of sample points
-  sp_xy = sim_plan_new(method_sp = method_sp, spread = spread, lims = lims, radius = d/2, 
+  sp_xy = sim_plan_new(method_sp = method_sp, spread = spread, lims = lims, radius = sp_radius, 
                        container = container, compartment = compartment, type = type)
   
   # Generate the distance matrix
@@ -15,10 +15,10 @@ sim_intmed_dis = function(n_contam, lims, spread, covar_mat, n_affected, spread_
   
   # Generate combined dataset
   contam_sp_xy = gen_sim_data_new(df_contam = contam_xy, df_sp = sp_xy, dist = dist_contam_sp, 
-                                  spread = spread, d = d, L = L, rho = rho, m_kbar = m_kbar, conc_neg = conc_neg)
+                                  spread = spread, sp_radius = sp_radius, L = L, rho = rho, m_kbar = m_kbar, conc_neg = conc_neg)
   
   # Create work portion and test portion
-  sample_dis = get_sample_dis(data = contam_sp_xy, container = container, m_kbar = m_kbar, tox = tox)
+  sample_dis = get_sample_dis(data = contam_sp_xy$raw$c_pooled, container = container, m_kbar = m_kbar, tox = tox)
   
   return(list(contam_sp_xy = contam_sp_xy, dist = dist_contam_sp, sample = sample_dis))
 }
@@ -48,8 +48,8 @@ sim_intmed_cont = function(n_contam, lims, spread, spread_radius, cont_level,
 
 # Create intermediate datasets
 sim_intmed = function(n_contam, lims, spread, covar_mat, n_affected, spread_radius, dis_level,
-                      method_sp, d, container, compartment, type, L, rho, m_kbar, conc_neg, tox, 
-                      n_sp, sp_radius, n_strata, by, LOC, fun){
+                      method_sp, sp_radius, container, compartment, type, L, rho, m_kbar, conc_neg, tox, 
+                      n_sp, n_strata, by, LOC, fun){
   
   # Check point
   stopifnot(spread %in% c("discrete", "continuous"))
@@ -58,7 +58,7 @@ sim_intmed = function(n_contam, lims, spread, covar_mat, n_affected, spread_radi
     
     sim_intmed_dis(n_contam = n_contam, lims = lims, spread = spread, covar_mat = covar_mat, 
                    n_affected = n_affected, dis_level = dis_level, 
-                   method_sp = method_sp, d = d, container = container, compartment = compartment, 
+                   method_sp = method_sp, sp_radius = sp_radius, container = container, compartment = compartment, 
                    type = type, L = L, rho = rho, m_kbar = m_kbar, conc_neg = conc_neg, tox = tox)
   } else {
     
@@ -66,4 +66,29 @@ sim_intmed = function(n_contam, lims, spread, covar_mat, n_affected, spread_radi
                     cont_level = cont_level, method_sp = method_sp, n_sp = n_sp, 
                     n_strata = n_strata, by = by, LOC = LOC, fun = fun)
   }
+}
+
+# Outcome simulation for continuous case
+sim_outcome_cont = function(n_contam, lims, spread, spread_radius, method_sp, 
+                            n_sp, n_strata, by, LOC, fun, case, m, M, method_det){
+  
+  # Create intermediate datasets
+  a = sim_intmed(n_contam = n_contam, lims = lims, spread = spread, spread_radius = spread_radius, 
+                 method_sp = method_sp, n_sp = n_sp, n_strata = n_strata, by = by, LOC = LOC, fun = fun)
+  
+  # Extract intermediate datasets
+  contam_sp_xy = a$contam_sp_xy
+  dist_contam_sp = a$dist
+  
+  # Determine which contamination points are detected or which sample points detect contamination
+  cover = calc_cover(df_dist = dist_contam_sp, spread_radius = spread_radius, sp_radius = sp_radius, spread = spread)
+  
+  ## Determine whether the contamination is detected or not. If detected, I_det = 1; if not detected, I_det = 0.
+  I_det = {nrow(cover) > 0} %>% as.integer()
+  
+  ## Determine lot decision
+  decision = lot_decision_new(data = contam_sp_xy, case = case, m = m, M = M, spread = spread, method_det = method_det)
+  
+  # Output
+  list(I_det, decision)
 }
