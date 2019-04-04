f1bbe63996dec0f18a814d72cddf95c196358539
ericxbcheng <xianbin2@illinois.edu>
2019-04-04 17:56:33 -0500

    Updates: 1. Add do_rej() 2. Clean the checkpoints in sim_iterate() 3. Rewrite calc_Prej() so that it includes do_rej()

--- Sampling_iteration.R
+++ Sampling_iteration.R
@@ -31,15 +31,23 @@ clean = function(data){
   return(b)
 }
 
-# Calculate the probability of rejection
-calc_Prej = function(decision){
-  
+# Determine if we accept or reject a lot
+do_rej = function(decision){
   # Check point
   stopifnot(decision %in% 1:7)
-
+  
   # Based on the lookup table, numbers 2,3,6 mean rejection while the others mean acceptance. 
   # Convert the decision vector into a logical vector. 1 = rejection, 0 = acceptance.
   a = {decision %in% c(2, 3, 6)}
+  
+  return(a)
+}
+
+# Calculate the probability of rejection
+calc_Prej = function(decision){
+  
+  # Determine whether to reject or accept a lot
+  a = do_rej(decision = decision)
   
   # Calculate the probability of rejection
   b = mean(a)
@@ -61,10 +69,8 @@ calc_Pdet = function(I_det){
 # Create a function that can iterate the simulation for n_iter times
 sim_iterate = function(n_iter, fun){
   
-  # Check point: Is n_iter >= 1 and is it an integer?
-  if(! any(n_iter >=1, is.integer(integer))){
-    stop("n_iter is either not >= 1 or not an integer.")
-  }
+  # Check point: Is n_iter >= 1?
+  stopifnot(n_iter >= 1)
   
   # Iterate the function for n_iter times
   map(.x = 1:n_iter, .f = fun)
