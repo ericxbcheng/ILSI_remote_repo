8c1c4a2230e8f507d185ac661a67113d0b1728aa
ericxbcheng <xianbin2@illinois.edu>
2019-04-25 18:26:41 -0500

    Add "bg_level" in gen_sim_outcome_new()

--- Sampling_iteration.R
+++ Sampling_iteration.R
@@ -1,36 +1,3 @@
-# A function factory that produces sim_outcome()'s given all the input parameters
-gen_sim_outcome = function(n_contam, xlim, ylim, n_affected, covar_mat, spread_radius,
-                           method_sp, n_sp, sp_radius, spread, n_strata, by, cont_level, 
-                           LOC, fun, m_kbar, m_sp, conc_good, case, m, M, Mc, method_det){
-  function(...){
-    sim_outcome(n_contam = n_contam, xlim = xlim, ylim = ylim, n_affected = n_affected,
-                covar_mat = covar_mat, spread_radius = spread_radius, method_sp = method_sp,
-                n_sp = n_sp, sp_radius = sp_radius, spread = spread, n_strata = n_strata, 
-                by = by, cont_level = cont_level, LOC = LOC, fun = fun, m_kbar = m_kbar, 
-                m_sp = m_sp, conc_good = conc_good, case = case, m = m, M = M, 
-                Mc = Mc, method_det = method_det)
-  }
-}
-
-# Rearrange the data in the nested list so that data of the same category are grouped together.
-clean = function(data){
-  # Convert the data into a vector
-  a = unlist(data)
-  
-  #Produce a sequence of indices for element extraction
-  ind = seq(from = 1, by = 3, length.out = length(a)/3)
-  
-  # Extract elements respectively
-  I_det = a[ind]
-  ROD = a[ind + 1]
-  decision = a[ind + 2]
-  
-  # Output
-  b = list("I_det" = I_det, "ROD" = ROD, "decision" = decision)
-  
-  return(b)
-}
-
 # Determine if we accept or reject a lot
 do_rej = function(decision){
   # Check point
