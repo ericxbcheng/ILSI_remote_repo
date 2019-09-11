528430d4880c695952ef5e8c526cb06c52ca0564
ericxbcheng <xianbin2@illinois.edu>
2019-09-11 16:50:20 -0500

    Add c_hat into gen_sim_outcome_new()

--- Sampling_iteration.R
+++ Sampling_iteration.R
@@ -1,9 +1,9 @@
 # A function factory
-gen_sim_outcome_new = function(n_contam, lims, spread, spread_radius, method_sp, n_sp, n_strata, 
+gen_sim_outcome_new = function(n_contam, c_hat, lims, spread, spread_radius, method_sp, n_sp, n_strata, 
                                by, LOC, fun, case, m, M, m_sp, method_det, covar_mat, n_affected, dis_level, cont_level, 
                                sp_radius, container, L, rho, m_kbar, conc_neg, tox, Mc, verbose = FALSE, bg_level, geom, seed, homogeneity){
   function(...){
-    sim_outcome_new(n_contam = n_contam, lims = lims, spread = spread, covar_mat = covar_mat,
+    sim_outcome_new(n_contam = n_contam, c_hat = c_hat, lims = lims, spread = spread, covar_mat = covar_mat,
                     n_affected = n_affected, dis_level = dis_level, method_sp = method_sp, sp_radius = sp_radius,
                     container = container, L = L, rho = rho, m_kbar = m_kbar, conc_neg = conc_neg, 
                     tox = tox, Mc = Mc, m_sp = m_sp, method_det = method_det, verbose = verbose, spread_radius = spread_radius, 
