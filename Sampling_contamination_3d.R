ccf0be4c93ac2fa94e3c008b912a92cc6efbd9d9
ericxbcheng <xianbin2@illinois.edu>
2019-10-29 15:29:29 -0500

    Added an error message to sim_contam_dis()

--- Sampling_contamination_3d.R
+++ Sampling_contamination_3d.R
@@ -301,7 +301,9 @@ sim_contam_dis = function(c_hat, lims, rho, m_kbar, dis_level, conc_neg, spread,
   
   # Calculate n_spot
   # Set the boundary for n_affected: 0 <= n_affected <= n_contam_total - 1
-  stopifnot(n_affected <= n_contam_total - 1)
+  if(n_affected <= n_contam_total - 1){
+    stop(cat("n_affected must be <=", n_contam_total - 1))
+  }
   
   n_spot = ceiling(n_contam_total / (1 + n_affected))
   
