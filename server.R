41b07b902935946faa8fc77757e9c21d520c76dd
ericxbcheng <xianbin2@illinois.edu>
2020-03-10 11:06:21 -0500

    Major updates: 1. Modify several inputs so that they are within conditional panels 2. Add iteration inputs

--- server.R
+++ server.R
@@ -6,7 +6,6 @@
 #
 #    http://shiny.rstudio.com/
 #
-
 library(shiny)
 library(shinydashboard)
 
@@ -27,60 +26,91 @@ source(file = "Sampling_analysis.R")
 
 vis_once = function(input, output, spread, ArgList){
   
-  observeEvent(eventExpr = {input$vis}, handlerExpr = {
+  if(spread == "continuous"){
     
-    if(spread == "continuous"){
-      
-      # Remove unnecessary arguments
-      ArgList_vis = ArgList
-      ArgList_vis[c("case", "M", "m_sp", "method_det")] = NULL
-      ArgList_vis$seed = NaN
-      print(ArgList_vis)
-      # Produce intermediate outputs
-      one_iteration = do.call(what = sim_intmed, args = ArgList_vis)
-      output$overlay_draw = renderPlot(expr = {overlay_draw(method_sp = ArgList_vis$method_sp, data = one_iteration[["contam_sp_xy"]] , 
-                                                            spread = ArgList_vis$spread, xlim = ArgList_vis$lims$xlim, ylim = ArgList_vis$lims$ylim, 
-                                                            n_strata = ArgList_vis$n_strata, by = ArgList_vis$by)})
-      output$contam_level_draw = renderPlot(expr = {contam_level_draw(dimension = "3d", method = ArgList_vis$fun, 
-                                                                      spread_radius = ArgList_vis$spread_radius, LOC = ArgList_vis$LOC, 
-                                                                      df_contam = one_iteration[["contam_sp_xy"]] , xlim = ArgList_vis$lims$xlim, 
-                                                                      ylim = ArgList_vis$lims$ylim, bg_level = ArgList_vis$bg_level, 
-                                                                      geom = ArgList_vis$geom)})
-      
-    } else {
-      message("Under construction.") 
-    }
-  })
+    # Remove unnecessary arguments
+    ArgList_vis = ArgList
+    ArgList_vis[c("case", "M", "m_sp", "method_det")] = NULL
+    ArgList_vis$seed = NaN
+    print(ArgList_vis)
+    # Produce intermediate outputs
+    one_iteration = do.call(what = sim_intmed, args = ArgList_vis)
+    output$overlay_draw = renderPlot(expr = {overlay_draw(method_sp = ArgList_vis$method_sp, data = one_iteration[["contam_sp_xy"]] , 
+                                                          spread = ArgList_vis$spread, xlim = ArgList_vis$lims$xlim, ylim = ArgList_vis$lims$ylim, 
+                                                          n_strata = ArgList_vis$n_strata, by = ArgList_vis$by)})
+    output$contam_level_draw = renderPlot(expr = {contam_level_draw(dimension = "3d", method = ArgList_vis$fun, 
+                                                                    spread_radius = ArgList_vis$spread_radius, LOC = ArgList_vis$LOC, 
+                                                                    df_contam = one_iteration[["contam_sp_xy"]] , xlim = ArgList_vis$lims$xlim, 
+                                                                    ylim = ArgList_vis$lims$ylim, bg_level = ArgList_vis$bg_level, 
+                                                                    geom = ArgList_vis$geom)})
+    
+  } else {
+    message("Under construction.") 
+  }
+}
+
+load_once = function(input, output){
+  
+  if(input$sidebarMenu == "2D"){
+    spread = "continuous"
+    
+    ArgList_default = list(n_contam = input$n_contam, lims = list(xlim = c(0, input$x_lim), ylim = c(0, input$y_lim)),
+                           spread = spread, spread_radius = input$spread_radius,
+                           cont_level = c(input$cont_level_mu, input$cont_level_sd), method_sp = input$method_sp,
+                           n_sp = input$n_sp, n_strata = input$n_strata, by = input$by, LOC = input$LOC,
+                           fun = input$fun, case = input$case, m = input$m, M = input$M, m_sp = input$m_sp,
+                           method_det = input$method_det, bg_level = input$bg_level, geom = input$geom)
+    
+  } else if(input$sidebarMenu == "3D"){
+    spread = "discrete"
+  } else {
+    stop("Unknown manual mode")
+  }
+  return(list(spread = spread, ArgList_default = ArgList_default))
+}
+
+# Iterate the model without any tuning parameters
+iterate_tune0 = function(input, output, Args){
+  
+  output$data_raw = sim_iterate2(n_seed = input$n_seed, n_iter = input$n_iter, Args = Args)
+  
+}
+
+# Parse the string "a,b,c" from val_prim into a numeric vector c(a,b,c)
+parse_num_vec = function(string){
+  return(str_split(string = string, pattern = ",") %>%
+           unlist() %>%
+           as.numeric())
+}
+
+# Iterate the model with one tuning parameter
+iterate_tune1 = function(input, output, Args){
+  
+  vals = parse_num_vec(string = input$val_prim)
+  
+  output$data_raw = map(.x = vals, .f = tune_param, Args = ArgList_default, n_seed = input$n_seed, n_iter = input$n_iter, param = input$var_prim)
+  
 }
 
 
 
 shinyServer(function(input, output) {
   
-  # Load the parameters
+  a = list()
+  
+  # Load the parameter once
   observeEvent(eventExpr = {input$load}, handlerExpr = {
-    
-    if(input$sidebarMenu == "2D"){
-      spread = "continuous"
-      
-      ArgList_default = list(n_contam = input$n_contam, lims = list(xlim = c(0, input$x_lim), ylim = c(0, input$y_lim)),
-                             spread = spread, spread_radius = input$spread_radius,
-                             cont_level = c(input$cont_level_mu, input$cont_level_sd), method_sp = input$method_sp,
-                             n_sp = input$n_sp, n_strata = input$n_strata, by = input$by, LOC = input$LOC,
-                             fun = input$fun, case = input$case, m = input$m, M = input$M, m_sp = input$m_sp,
-                             method_det = input$method_det, bg_level = input$bg_level, geom = input$geom)
-      
-      vis_once(input = input, output = output, spread = spread, ArgList = ArgList_default)
-      
-    } else if(input$sidebarMenu == "3D"){
-      spread = "discrete"
-    } else {
-      stop("Unknown manual mode")
-    }
-    
+    a <<- load_once(input = input, output = output)
+    output$print_param = renderPrint(expr = a$ArgList_default)
   })
   
   # Visualize for one iteration
+  observeEvent(eventExpr = {input$vis}, handlerExpr = {
+    vis_once(input = input, output = output, spread = a$spread, ArgList = a$ArgList_default)
+  })
   
-  
+  observeEvent(eventExpr = {input$val_prim},handlerExpr = {
+    b = parse_num_vec(string = input$val_prim)
+    print(b)
+  })
 })
--- ui.R
+++ ui.R
@@ -10,118 +10,150 @@
 library(shiny)
 library(shinydashboard)
 
-# v_manual_3d = sidebarLayout(
-#     sidebarPanel(
-#         numericInput(inputId = "c_hat", label = "Estimated bulk contamination level (ppb)", value = 1, min = 1, step = 1),
-#         numericInput(inputId = "x_lim", label = "Length (m)", value = 10, min = 1),
-#         numericInput(inputId = "y_lim", label = "Width (m)", value = 10, min = 1),
-#         numericInput(inputId = "z_lim", label = "Height (m)", value = 10, min = 1)
-#         
-#     ),
-#     mainPanel()
-# )
-
 sidebar = dashboardSidebar(
-    
-    sidebarMenu(
-        id = "sidebarMenu",
-        menuItem(text = "Introduction", 
-                 tabName = "intro"),
-        menuItem(text = "Inputs", 
-                 menuItem(text = "Smart Version", 
-                             tabName = "v_smart"), 
-                 menuItem(text = "Manual Version", 
-                             tabName = "v_manual", 
-                          menuSubItem(text = "2D", tabName = "2D"), 
-                          menuSubItem(text = "3D", tabName = "3D")),
-                 tabName = "inputs"),
-        menuItem(text = "Outputs", 
-                 menuSubItem(text = "Visualization", tabName = "vis"),
-                 menuSubItem(text = "Data Export", tabName = "export"),
-                 tabName = "outputs")
-    )
+  
+  sidebarMenu(
+    id = "sidebarMenu",
+    menuItem(text = "Introduction", 
+             tabName = "intro"),
+    menuItem(text = "Inputs", 
+             menuItem(text = "Smart Version", 
+                      tabName = "v_smart"), 
+             menuItem(text = "Manual Version", 
+                      tabName = "v_manual", 
+                      menuSubItem(text = "2D", tabName = "2D"), 
+                      menuSubItem(text = "3D", tabName = "3D")),
+             tabName = "inputs"),
+    menuItem(text = "Outputs", 
+             menuSubItem(text = "Visualization", tabName = "vis"),
+             menuSubItem(text = "Data Export", tabName = "export"),
+             tabName = "outputs")
+  )
 )
 
 v_manual_2D = fluidRow(
-    
-    box(title = "2D Input Parameters",
-        selectInput(inputId = "geom", label = "Geometry", choices = list("Point-source" = "point", "Area-based" = "area"), multiple = FALSE),
-        conditionalPanel(
-            condition = "input.geom == 'point'",
-            numericInput(inputId = "n_contam", label = "Number of contamination points", value = 1, min = 1, step = 1)
-        ),
+  
+  box(title = "2D Input Parameters",
+      splitLayout(
         numericInput(inputId = "x_lim", label = "Length (m)", value = 10, min = 1),
-        numericInput(inputId = "y_lim", label = "Width (m)", value = 10, min = 1),
+        numericInput(inputId = "y_lim", label = "Width (m)", value = 10, min = 1)
+      ),
+      selectInput(inputId = "geom", label = "Geometry", choices = list("Point-source" = "point", "Area-based" = "area"), multiple = FALSE),
+      conditionalPanel(
+        condition = "input.geom == 'point'",
+        numericInput(inputId = "n_contam", label = "Number of contamination points", value = 1, min = 1, step = 1),
+        numericInput(inputId = "spread_radius", label = "Radius of contamination area (m)", value = 1, min = 0)
+      ),
+      splitLayout(
         numericInput(inputId = "cont_level_mu", label = "Mean contamination level (log CFU/g)", value = 3),
-        numericInput(inputId = "cont_level_sd", label = "Standard deviation of contamination level (log CFU/g)", value = 1),
-        numericInput(inputId = "bg_level", label = "Background level (CFU/g)", value = 0.00001, min = 0),
-        numericInput(inputId = "spread_radius", label = "Radius of contamination area (m)", value = 1, min = 0),
-        numericInput(inputId = "LOC", label = "LOC", value = 0.001),
-        selectInput(inputId = "fun", label = "Decay function", choices = list("Exponential" = "exp", "Gaussian" = "norm", "Uniform" = "unif")),
-        numericInput(inputId = "n_sp", label = "Number of sample points", value = 5, min = 1),
-        selectInput(inputId = "method_sp", label = "Sampling strategy", choices = list("SRS" = "srs", "STRS" = "strs", "k-step SS" = "ss")),
+        numericInput(inputId = "cont_level_sd", label = "Standard deviation of contamination level (log CFU/g)", value = 1)
+      ),
+      numericInput(inputId = "bg_level", label = "Background level (CFU/g)", value = 0.00001, min = 0),
+      selectInput(inputId = "fun", label = "Decay function", choices = list("Exponential" = "exp", "Gaussian" = "norm", "Uniform" = "unif")),
+      conditionalPanel(
+        condition = "input.fun != 'unif'",
+        numericInput(inputId = "LOC", label = "Limit of contamination contribution", value = 0.001, min = 0, max = 1)
+      ),
+      numericInput(inputId = "n_sp", label = "Number of sample points", value = 5, min = 1, step = 1),
+      selectInput(inputId = "method_sp", label = "Sampling strategy", choices = list("SRS" = "srs", "STRS" = "strs", "k-step SS" = "ss")),
+      conditionalPanel(
+        condition = "input.method_sp == 'strs'",
+        selectInput(inputId = "by", label = "Stratify by", choices = list("Row" = "row", "Column" = "column", "2D" = "2d")),
         conditionalPanel(
-            condition = "input.method_sp == 'strs'",
-            selectInput(inputId = "by", label = "Stratify by", choices = list("Row" = "row", "Column" = "column", "2D" = "2d")),
-            conditionalPanel(
-                condition = "input.by != '2d'",
-                numericInput(inputId = "n_strata", label = "Number of strata", value = NULL, min = 1)
-            ),
-            conditionalPanel(
-                condition = "input.by == '2d'",
-                numericInput(inputId = "n_strata_row", label = "Number of strata (row)", value = NULL, min = 1),
-                numericInput(inputId = "n_strata_col", label = "Number of strata (column)", value = NULL, min = 1)
-            )
+          condition = "input.by != '2d'",
+          numericInput(inputId = "n_strata", label = "Number of strata", value = NULL, min = 1)
         ),
         conditionalPanel(
-            condition = "input.method_sp == 'ss'",
-            selectInput(inputId = "by", label = "By", choices = list("Row" = "row", "Column" = "column"))
-        ),
-        numericInput(inputId = "m_sp", label = "Individual sample mass (g)", value = 25, min = 0),
-        selectInput(inputId = "method_det", label = "Detection method", choices = list("Plating", "Enrichment")),
-        sliderInput(inputId = "case", label = "Case", min = 1, value = 10, max = 15, step = 1, round = TRUE),
-        numericInput(inputId = "m", label = "m", value = NULL, min = 0),
-        conditionalPanel(
-            condition = "input.case < 10",
-            numericInput(inputId = "M", label = "M", value = NULL, min = 0)
-        ),
-        numericInput(inputId = "n_iter", label = "Number of iteration", value = 10, min = 1, step = 1),
-        actionButton(inputId = "load", label = "Load parameters"),
-        h2(),
-        conditionalPanel(
-            condition = "input.load == true",
-            splitLayout(
-                actionButton(inputId = "vis", label = "Visualize"),
-                actionButton(inputId = "iteration", label = "Iterate")
-            )
+          condition = "input.by == '2d'",
+          numericInput(inputId = "n_strata_row", label = "Number of strata (row)", value = NULL, min = 1),
+          numericInput(inputId = "n_strata_col", label = "Number of strata (column)", value = NULL, min = 1)
         )
-    ),
-    
-    box(title = "Visualization for one iteration",
-        plotOutput(outputId = "overlay_draw"),
-        plotOutput(outputId = "contam_level_draw")
-    )
+      ),
+      conditionalPanel(
+        condition = "input.method_sp == 'ss'",
+        selectInput(inputId = "by", label = "By", choices = list("Row" = "row", "Column" = "column"))
+      ),
+      numericInput(inputId = "m_sp", label = "Individual sample mass (g)", value = 25, min = 0),
+      selectInput(inputId = "method_det", label = "Detection method", choices = list("Plating", "Enrichment")),
+      sliderInput(inputId = "case", label = "Case", min = 1, value = 10, max = 15, step = 1, round = TRUE),
+      numericInput(inputId = "m", label = "m", value = NULL, min = 0),
+      conditionalPanel(
+        condition = "input.case < 10",
+        numericInput(inputId = "M", label = "M", value = NULL, min = 0)
+      ),
+      h2(),
+      splitLayout(
+        numericInput(inputId = "n_seed", label = "Number of contamination patterns", value = 1, min = 1, step = 1),
+        numericInput(inputId = "n_iter", label = "Number of sampling patterns per contamination pattern", value = 1, min = 1, step = 1)
+      ),
+      selectInput(inputId = "n_vars", label = "Number of tuning parameters", choices = list(0,1,2), multiple = FALSE),
+      conditionalPanel(
+        condition = "input.n_vars == 1",
+        selectInput(inputId = "var_prim", 
+                    label = "Primary tuning parameter", 
+                    choices = list("Number of contamination points" = "n_contam",
+                                   "Number of sample points" = "n_sp",
+                                   "Individual sample mass (g)" = "m_sp")),
+        textInput(inputId = "val_prim", label = "Tuning value(s)", value = NULL)
+      ),
+      conditionalPanel(
+        condition = "input.n_vars == 2",
+        selectInput(inputId = "var_prim", 
+                    label = "Primary tuning parameter", 
+                    choices = list("Number of contamination points" = "n_contam",
+                                   "Number of sample points" = "n_sp",
+                                   "Individual sample mass (g)" = "m_sp")),
+        textInput(inputId = "val_prim", label = "Tuning value(s)", value = NULL),
+        selectInput(inputId = "var_sec", 
+                    label = "Secondary tuning parameter", 
+                    choices = list("Sampling strategy" = "method_sp")),
+        textInput(inputId = "val_sec", label = "Tuning value(s)", value = NULL)
+      ),
+      actionButton(inputId = "load", label = "Load parameters"),
+      h2(),
+      conditionalPanel(
+        condition = "input.load > 0",
+        splitLayout(
+          actionButton(inputId = "vis", label = "Visualize"),
+          actionButton(inputId = "iteration", label = "Iterate")
+        )
+      )
+  ),
+  
+  box(title = "Visualization for one iteration",
+      plotOutput(outputId = "overlay_draw"),
+      plotOutput(outputId = "contam_level_draw")
+  )
 )
 
+
+# Visualization page
+# page_vis = fluidRow(
+#   
+#   box(title = "Selected parameters", 
+#       verbatimTextOutput(outputId = "print_param")),
+#   box(title = "Visualization for multiple iterations")
+# )
+
+
 body = dashboardBody(
-    
-    tabItems(
-        tabItem(tabName = "intro", 
-                h2("This is the introduction page.")),
-        tabItem(tabName = "v_smart", 
-                h2("This is the smart version page.")),
-        tabItem(tabName = "2D", v_manual_2D),
-        tabItem(tabName = "3D", h2("3D inputs.")),
-        tabItem(tabName = "vis", 
-                h2("This is the visualization page.")),
-        tabItem(tabName = "export",
-                h2("This is the data export page."))
-    )
+  
+  tabItems(
+    tabItem(tabName = "intro", 
+            h2("This is the introduction page.")),
+    tabItem(tabName = "v_smart", 
+            h2("This is the smart version page.")),
+    tabItem(tabName = "2D", v_manual_2D),
+    tabItem(tabName = "3D", h2("3D inputs.")),
+    tabItem(tabName = "vis", h2("This is the visualization page")),
+    tabItem(tabName = "export",
+            h2("This is the data export page."))
+  )
 )    
-    
+
 shinyUI(dashboardPage(
-    
-    dashboardHeader(title = "2D and 3D Sampling Simulation"),
-    sidebar,
-    body
+  
+  dashboardHeader(title = "2D and 3D Sampling Simulation"),
+  sidebar,
+  body
 ))
