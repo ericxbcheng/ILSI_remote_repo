24609951a99831344f47ec13272ec9cf185732c2
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2020-06-29 11:10:28 -0500

    Adding more action buttons to the GIU

--- R/Documentation_Pop_ups.R
+++ R/Documentation_Pop_ups.R
@@ -2,9 +2,55 @@
 
 observeEvent(input$Doc_Bins, {
     showModal(modalDialog(
-      title = "Documentatation for Bins",
-      includeHTML("R/Docs.html"),
+      title = "Documentatation: Sizes of Storage equipment",
+      includeHTML("R/Documentation Documents/html sizes of storage.html"),
       easyClose = TRUE
       ,size = "l"
     ))
   })
+
+
+observeEvent(input$Doc_Levels, {
+  showModal(modalDialog(
+    title = "Documentatation: Permitted mycotoxin levels",
+    includeHTML("R/Documentation Documents/Mycotoxin Levels.html"),
+    easyClose = TRUE
+    ,size = "l"
+  ))
+})
+
+observeEvent(input$Doc_Distributions, {
+  showModal(modalDialog(
+    title = "Documentatation: Effect of Distributions",
+    includeHTML("R/Documentation Documents/Distributions.html"),
+    easyClose = TRUE
+    ,size = "l"
+  ))
+})
+
+observeEvent(input$Doc_Clusters, {
+  showModal(modalDialog(
+    title = "Documentatation: Effect of clustering",
+    includeHTML("R/Documentation Documents/Clusters.html"),
+    easyClose = TRUE
+    ,size = "l"
+  ))
+})
+
+observeEvent(input$Doc_Sampling, {
+  showModal(modalDialog(
+    title = "Documentatation: Effect of Sampling Strategies",
+    includeHTML("R/Documentation Documents/Sampling.html"),
+    easyClose = TRUE
+    ,size = "l"
+  ))
+})
+
+observeEvent(input$Doc_Probes, {
+  showModal(modalDialog(
+    title = "Documentatation: Probe Sizes",
+    includeHTML("R/Documentation Documents/Probes.html"),
+    easyClose = TRUE
+    ,size = "l"
+  ))
+})
--- ui.R
+++ ui.R
@@ -41,16 +41,20 @@ v_manual_3D = fluidRow(
       numericInput(inputId = "x_lim_3d", label = "Length (m)", value = 3, min = 1),
       numericInput(inputId = "y_lim_3d", label = "Width (m)", value = 2, min = 1),
       numericInput(inputId = "z_lim_3d", label = "Height (m)", value = 1.5, min = 1),
-      actionButton("Doc_Bins", "",width = "20%",icon = icon("question-circle") ) 
+      actionButton("Doc_Bins", "",width = "40px",icon = icon("question-circle") ) 
     ),
     splitLayout(
       selectInput(inputId = "tox", label = "Mycotoxin", choices = list("Aflatoxin" = "AF")),
-      numericInput(inputId = "c_hat", label = "Overall mycotoxin level (ppb)", value = 1, min = 0.001)
+      numericInput(inputId = "c_hat", label = "Overall mycotoxin level (ppb)", value = 1, min = 0.001),
+      actionButton("Doc_Levels", "",width = "40px",icon = icon("question-circle") ) 
     ),
+    
+    actionButton("Doc_Distributions", "",width = "40px",icon = icon("question-circle") ),
     selectInput(inputId = "dis_level_type", 
                 label = "Mycotoxin distribution in contaminated grains", 
                 choices = list("Uniform" = "constant", "Gamma" = "Gamma"), 
                 multiple = FALSE),
+    
     conditionalPanel(condition = "input.dis_level_type == 'constant'",
                      numericInput(inputId = "dis_level_const_arg", 
                                   label = "Mycotoxin level in contaminated grains(ppb)", 
@@ -65,6 +69,7 @@ v_manual_3D = fluidRow(
                                     min = 0.001)
                        )
                      ),
+    actionButton("Doc_Clusters", "",width = "40px",icon = icon("question-circle") ),
     numericInput(inputId = "n_affected", label = "Number of grains in a cluster", value = 0, min = 0, step = 1),
     conditionalPanel(condition = "input.n_affected > 0",
                      wellPanel(
@@ -85,6 +90,7 @@ v_manual_3D = fluidRow(
                          numericInput(inputId = "vcov_33", label = NULL, value = 0.0004, min = 0)
                        )
                      )),
+    actionButton("Doc_Sampling", "",width = "40px",icon = icon("question-circle") ),
     selectInput(inputId = "method_sp_3d", label = "Sampling strategy", choices = list("SRS" = "srs", "STRS" = "strs", "SS" = "ss")),
     wellPanel(
       numericInput(inputId = "n_sp_3d", label = "Number of probes", value = 5, min = 1),
@@ -104,6 +110,7 @@ v_manual_3D = fluidRow(
                                    choices = list("Open-top" = "open_top", "Trough" = "trough")))
       
     ),
+    actionButton("Doc_Probes", "",width = "40px",icon = icon("question-circle") ),
     numericInput(inputId = "d", label = "Probe diameter (m)", value = 0.04, min = 0.01, max = 1, step = 0.01),
     splitLayout(
       numericInput(inputId = "m_kbar", label = "Single kernel mass (g)", value = 0.3, min = 0.001),
